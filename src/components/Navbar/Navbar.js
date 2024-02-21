9d37c1849c2b08641f94e66b7cdb17ce6e56b852
ManuAsta <b.manoj1998.bm@gmail.com>
2024-02-21 18:42:52 +0530

    final commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//styling
+import "./Navbar.css";
+
+
+
+
+export default function Navbar() {
+  return (
+    <>
+      <div id='navbar'>
+        <img src="album.png" alt='Logo' width={"50px"} height={"50px"}/>
+        <h3>Album Manager</h3>
+      </div>
+    </>
+  )
+}
